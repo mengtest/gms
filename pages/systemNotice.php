63a5f3e38e6be53a0eb4c3a73d3f8964e5c140ae
xmy_zjl <zhng523@163.com>
2017-09-30 17:54:00 +0800

    新增发送通告

---
+++ pages/systemNotice.php
@@ -0,0 +1,122 @@
+<?php
+	// 屏蔽提示
+	error_reporting(E_ALL || ~E_NOTICE);
+
+	$Title = "这里是系统通告页";
+	require_once("../html/header.html");
+	require_once("../config/menu.php");
+
+	// 检测登陆状态
+	require_once("../script/check.php");
+	$checkVal = JurisdictionCheck(basename($_SERVER["PHP_SELF"]), $_SESSION[uid]);
+	if ($checkVal != 0) {
+		echo "<script language=javascript>alert('$error_notice[$checkVal]');history.back();</script>";
+	}
+
+	require_once("../config/DBList.php");
+	require_once("../script/selectserverbat.php");
+?>
+
+<hr class = 'notice_hr' />
+
+<div class = 'notice_div'>
+	<form action = '' method = 'post'>
+		平台列表：<select name='notice_some'>
+			<option value = '0'>选择后服务器号将无效</option>
+			<option value = '1'>全平台</option>
+			<?php
+				$i = 2;
+				foreach ($serverList as $plat => $idList) {
+					echo "<option value = '$i'>$plat</option>";
+					$i++;
+				}
+			?>
+		</select><br/><br/><br/>
+		<table width="100%">
+			<tr>服务器号：<input name="notice_sid" type="text" value = "<?php echo $_SESSION[select_list]; ?>" style = "height:50px;width:85%" readonly/><tr><br/><br/><br/>
+			<tr>内&nbsp;&nbsp;容：<input name="notice_content" type="text" style = "height:50px;width:85%" /></tr><br/><br/><br/>
+			<tr><th>开始时间：<input name="notice_time" type="text" title = "1.小于1亿的数(多少秒后通告);2.时间戳" /></th><th>发送条数：<input name="notice_count" type="text" /></th><th>执行间隔：<input name="notice_interval" type="text" /></th></tr>
+		</table><br/><br/>
+		<div align = "center">
+			<input name = 'submitnotice' type = 'submit' value = '发送通告' />
+		</div>
+	</form>
+</div>
+
+<?php
+	echo "<br>";
+	echo "<br>";
+	echo "<br>";
+	function SendNotice($db_index, $sql_front, $sql_end) {
+		// 无效的直接过滤
+		if ($db_index > 0 && $sql_front && $sql_end) {
+			$conn = GetDBByIndex($db_index);
+			$serverId = GetServerId($db_index);
+
+			if ($conn && $serverId > 0) {
+				// 拼接sql
+				$sql = $sql_front.$serverId.$sql_end;
+				mysqli_query($conn, $sql);
+			}
+		}
+	}
+
+	if ($_POST[submitnotice]) {
+		if ($_POST[notice_content] && $_POST[notice_time] >= 0 && $_POST[notice_interval] >= 0 && $_POST[notice_count] >= 0) {
+			$sqlFront = "insert into gmcommand(worldid, type, command, param) values('";
+			$sqlEnd = "', '5', '$_POST[notice_content]', '";
+			$sqlEnd .= ($_POST[notice_time] ? $_POST[notice_time].',' : '0,');
+			$sqlEnd .= ($_POST[notice_interval] ? $_POST[notice_interval].',' : '1,');
+			$sqlEnd .= ($_POST[notice_count] ? $_POST[notice_count]."')" : "1')");
+
+			if ($_POST[some]) {
+				// 选了服或者平台,不读服务器号了
+				if ($_POST[some] == 0) {
+					foreach ($serverList as $plat => $idList) {
+						foreach ($idList as $serverid => $servername) {
+							SendNotice($serverid, $sqlFront, $sqlEnd);
+						}
+					}
+
+					$_SESSION[select_list] = '';
+				}
+				else {
+					$i = 2;
+					foreach ($serverList as $plat => $idList) {
+						if ($_POST[some] == $i) {
+							foreach ($idList as $serverid => $servername) {
+								SendNotice($serverid, $sqlFront, $sqlEnd);
+							}
+							break;
+						}
+
+						$i++;
+					}
+
+					$_SESSION[select_list] = '';
+				}
+			}
+			else {
+				if ($_POST[notice_sid]) {
+					// 去掉最后一个字符
+					$_POST[notice_sid] = substr($_POST[notice_sid], 0, strlen($_POST[notice_sid]) - 1);
+					$server_arr = explode(" ,", $_POST[notice_sid]); 
+
+					for ($i = 0; $i < count($server_arr); $i++) {
+						SendNotice(substr($server_arr[$i], 1), $sqlFront, $sqlEnd);
+					}
+
+					$_SESSION[select_list] = '';
+				}
+				else {
+					alertMsg("请先选择通告服");
+				}
+			}
+		}
+		else {
+			alertMsg("请先输入通告信息");
+		}
+	}
+
+	require_once("../html/bottom.html");
+?>
---
+++ script/selectserverbat.php
@@ -0,0 +1,112 @@
+<?php
+	// 屏蔽提示
+	error_reporting(E_ALL || ~E_NOTICE);
+
+	$absolute_path = dirname(dirname(__FILE__));
+
+	require_once("$absolute_path/config/CommomConfig.php");
+	require_once("$absolute_path/config/DBList.php");
+?>
+
+<div class = 'select_div'>
+<?php
+	if ($_SESSION[page] == null) {
+		$_SESSION[page] = 1;
+	}
+
+	// 每次都先重置总页数
+	$_SESSION[totalPage] = 1;
+
+	//function FlushPaltInfo() {
+		$i = 0;
+		foreach ($serverList as $plat => $idList) {
+			if ($i % plat_page == 0) {
+				$isShow = (($_SESSION[page] - 1) == ($i / plat_page)) ? 'block' : 'none';
+				echo "<div style='display:$isShow;'><table width = '60%'>";
+
+				if (($i / plat_page) >= $_SESSION[totalPage]) {
+					$_SESSION[totalPage] = ($i / plat_page) + 1;
+				}
+			}
+
+			if ($i % plat_num == 0) {
+				echo "<tr>";
+			}
+
+			echo "<th> $plat";
+			echo "<form action='' method='POST'> <select name='some'>";
+				foreach ($idList as $serverid => $servername) {
+					echo "<option value='$serverid'>$servername</option>";
+				}
+			echo "<input name='submitse$i' type='submit' value='提交' />";
+			echo "</select></form></th>";
+
+			if ($i % plat_num == (plat_num - 1)) {
+				echo "</tr>";
+			}
+
+			if ($i % plat_page == (plat_page - 1)) {
+				echo "</table></div>";
+			}
+
+			$i++;
+		}
+
+		if ($i % plat_num != 0) {
+			echo "</tr>";
+		}
+
+		if ($i % plat_page != 0) {
+			echo "</table></div>";
+		}
+	//}
+	//FlushPaltInfo();
+?>
+</div>
+
+<div class = 'select_buttom' style = 'display:<?php echo $_SESSION[totalPage] > 1 ? "block" : "none"; ?>;'>
+	<form action = '' method = 'post'>
+		<input name = 'submitlast' type = 'submit' value = '上一页' />
+	</form>
+	<p align = 'center'><?php echo $_SESSION[page].'/'.$_SESSION[totalPage]; ?></p>
+	<form action = '' method = 'post'>
+		<input name = 'submitnext' type = 'submit' value = '下一页' />
+	</form>
+</div>
+
+<?php
+	$j = 0;
+	foreach ($serverList as $plat => $idList) {
+		$postname = 'submitse'.$j;
+		if ($_POST[$postname]) {
+			if ($_POST[some] > 0) {
+				$selcet_info = 's'.$_POST[some].' ';
+
+				if ($_SESSION[select_list] == null) {
+					$_SESSION[select_list] = $selcet_info;
+				}
+				elseif (!strstr($_SESSION[select_list], $selcet_info)) {
+					$_SESSION[select_list] .= ','.$selcet_info;
+				}
+			}
+
+			break;
+		}
+
+		$j++;
+	}
+
+	if ($_POST[submitlast]) {
+		if ($_SESSION[page] != null && $_SESSION[page] > 1) {
+			--$_SESSION[page];
+			echo "<script language = 'JavaScript'> location.reload() ; </script>";
+		}
+	}
+
+	if ($_POST[submitnext]) {
+		if ($_SESSION[page] != null && $_SESSION[totalPage] != null && $_SESSION[page] < $_SESSION[totalPage]) {
+			++$_SESSION[page];
+			echo "<script language = 'JavaScript'> location.reload() ; </script>";
+		}
+	}
+?>
