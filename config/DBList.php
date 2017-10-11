7ba7a5c99aff70bfcea4e3b1225b3ab990e027c5
xmy_zjl <zhng523@163.com>
2017-10-11 20:46:04 +0800

    db配置优化

--- config/DBList.php
+++ config/DBList.php
@@ -6,103 +6,88 @@
 	** ServerId可以重复(必须大于0)
 	** ----------------------------------------------------------------------------------
 	** 增加一个新服:
-	** 1.index+1，绑定一个数据库(复制前面的就好了，对应修改数据库地址和用户名密码)
-	** 2.绑定一个ServerId(WorldId)
-	** 3.绑定一个服名称(显示用)
+	** 1.填写服配置(自增索引)
+	** 2.填写服列表(每个服必须有对应的索引,索引必须唯一)
 	*/
 
-	// 获取数据库函数(1)
-	function GetDBByIndex($index) {
-		$conn = null;
+	// 服配置(1)
+	// 0为gm后台数据库配置
+	// 1之后为服所在数据库配置
+	$serverConfig = array(
+		0 => array(
+			// 数据库所在地址
+			'addr_s' => 'localhost',
+			// 数据库用户名
+			'user' => 'root',
+			// 数据库密码
+			'password' => 'root',
+			// 数据表名称
+			'DataSource' => 'test',
+			// 服id(也是游戏的worldid)
+			'serverId' => -1,
+			),
+		1 => array(
+			'addr_s' => 'localhost',
+			'user' => 'root',
+			'password' => 'root',
+			'DataSource' => '3dmmo',
+			'serverId' => 100,
+			),
+		2 => array(
+			'addr_s' => '192.168.1.117',
+			'user' => 'root',
+			'password' => 'root',
+			'DataSource' => 'mtlbbdb_100',
+			'serverId' => 106,
+			),
+		3 => array(
+			'addr_s' => '192.168.1.117',
+			'user' => 'root',
+			'password' => 'root',
+			'DataSource' => 'mtlbbdb_100',
+			'serverId' => 107,
+			),
+		4 => array(
+			'addr_s' => '192.168.1.117',
+			'user' => 'root',
+			'password' => 'root',
+			'DataSource' => 'mtlbbdb_100',
+			'serverId' => 108,
+			),
+		5 => array(
+			'addr_s' => '192.168.1.117',
+			'user' => 'root',
+			'password' => 'root',
+			'DataSource' => 'mtlbbdb_100',
+			'serverId' => 109,
+			),
+		6 => array(
+			'addr_s' => '192.168.1.117',
+			'user' => 'root',
+			'password' => 'root',
+			'DataSource' => 'mtlbbdb_100',
+			'serverId' => 110,
+			),
+		7 => array(
+			'addr_s' => '192.168.1.117',
+			'user' => 'root',
+			'password' => 'root',
+			'DataSource' => 'mtlbbdb_100',
+			'serverId' => 111,
+			),
+		8 => array(
+			'addr_s' => '192.168.1.117',
+			'user' => 'root',
+			'password' => 'root',
+			'DataSource' => 'mtlbbdb_100',
+			'serverId' => 117,
+			),
+		);
 
-		switch ($index) {
-			case 0:
-			{
-				/*
-				** 设置DB信息(数据库地址， 用户名， 密码)
-				** 设置数据库名称
-				*/
-				$conn = mysqli_connect('localhost','root','root');
-				mysqli_select_db($conn, 'test');
-			}
-			break;
-
-			case 1:
-			{
-				$conn = mysqli_connect('localhost','root','root');
-				mysqli_select_db($conn, '3dmmo');
-			}
-			break;
-
-			case 2:
-			case 3:
-			case 4:
-			case 5:
-			case 6:
-			case 7:
-			case 8:
-			{
-				$conn = mysqli_connect('192.168.1.117','root','root');
-				mysqli_select_db($conn, 'mtlbbdb_100');
-			}
-			break;
-			
-			default:
-				break;
-		}
-
-		if ($conn == null) {
-			echo 'Could not connect:<br>' . mysqli_error();
-		}
-
-		return $conn;
-	}
-
-	// 获取游戏服id(2)
-	function GetServerId($index) {
-		$serverId = -1;
-
-		switch ($index) {
-			case 1:
-				$serverId = 100;
-				break;
-
-			case 2:
-				$serverId = 106;
-				break;
-
-			case 3:
-				$serverId = 107;
-				break;
-
-			case 4:
-				$serverId = 108;
-				break;
-
-			case 5:
-				$serverId = 109;
-				break;
-
-			case 6:
-				$serverId = 110;
-				break;
-
-			case 7:
-				$serverId = 111;
-				break;
-
-			case 8:
-				$serverId = 117;
-				break;
-			
-			default:
-				break;
-		}
-
-		return $serverId;
-	}
-
-	// 服列表(3)
+	// 服列表(2)
+	// serverList下的key代表平台名
+	// 平台下的key为索引,必须与上面服配置匹配
+	// 平台下value为服名称(显示用)
 	$serverList = array(
 		'本地服0' => array(
 			1 => 's100(本地)',
@@ -153,4 +138,35 @@
 			7 => 's111(内网)', 
 			), 
 		);
+
+
+	/*------------------------------------------------------分割线-------------------------------------------------------*/
+	/*--------------------------------------------------以下不需要修改---------------------------------------------------*/
+
+	function GetDBByIndex($index) {
+		$conn = null;
+
+		$servercfg = $GLOBALS[serverConfig][$index];
+		if ($servercfg && $servercfg['addr_s'] && $servercfg['DataSource']) {
+			$conn = mysqli_connect($servercfg['addr_s'], $servercfg['user'], $servercfg['password']);
+			mysqli_select_db($conn, $servercfg['DataSource']);
+		}
+
+		if ($conn == null) {
+			echo 'Could not connect:<br>' . mysqli_error();
+		}
+
+		return $conn;
+	}
+
+	function GetServerId($index) {
+		$serverId = -1;
+
+		$servercfg = $GLOBALS[serverConfig][$index];
+		if ($servercfg && $servercfg['addr_s'] && $servercfg['serverId']) {
+			$serverId = $servercfg['serverId'];
+		}
+
+		return $serverId;
+	}
 ?>
