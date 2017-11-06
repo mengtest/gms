3cbbdc14fcd8cd0fa72bb5c35f3677d19689b26a
xmy_zjl <zhng523@163.com>
2017-11-06 16:13:44 +0800

    新增查询平台角色接口

---
+++ pages/queryRoleByPlat.php
@@ -0,0 +1,41 @@
+<?php
+	$ResInfo = array(
+		'count' => 0,
+		'data' => array(),
+		);
+	if ($_GET['plat'] == '' || $_GET['plat'] == null ||
+		$_GET['accname'] == '' || $_GET['accname'] == null) {
+		echo json_encode($ResInfo);
+		exit();
+	}
+
+	require_once("../config/CommomConfig.php");
+	require_once("../config/DBList.php");
+
+	function AddResInfo($serverId, $name, $prof, $level) {
+		$tempInfo = array(
+			'sId' => $serverId,
+			'name' => $name,
+			'prof' => $prof,
+			'level' => $serverId
+			);
+		array_push($GLOBALS[ResInfo]['data'], $tempInfo);
+		$GLOBALS[ResInfo]['count'] += 1;
+	}
+
+	foreach ($serverConfig as $index => $value) {
+		if ($value['platName'] == $_GET['plat']) {
+			$conn = GetDBByIndex($index);
+			$serverId = GetServerId($index);
+			if ($conn != null && $serverId > 0) {
+				$sql = "select * from charfulldata where worldid = $serverId and accname = '$_GET[accname]'";
+				$query = mysqli_query($conn, $sql);
+				while ($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+					AddResInfo($serverId, $row[charname], $row[profession], $row[level]);
+				}
+			}
+		}
+	}
+
+	echo json_encode($ResInfo);
+?>
