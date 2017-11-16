b6e98ac9a360e0847b9ee9478e7ccd08cb509a3e
xmy_zjl <zhng523@163.com>
2017-11-16 17:53:27 +0800

    删除多余文件

--- pages/reyun.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include_once("../config/CommomConfig.php");
-?>
-
-<h2 style="position:fixed; left:2%; top:0px;"><a title="返回GM后台" href=<?php echo host_addr; ?>>RETURN GMS</a></h2>
-
-<iframe id="mainFrame" name="mainFrame" scrolling="no" src="http://game.reyun.com" frameborder="0" style="padding: 0px; width: 100%; height: 1000px;"></iframe>
